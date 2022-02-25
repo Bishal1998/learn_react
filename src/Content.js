8dc68b66edb184942cc961df22f01f93077dc7f1
Bishal1998 <Bishalpaudel1998@gmail.com>
2022-02-25 10:47:20 +0545

    Click Event

--- src/Content.js
+++ src/Content.js
@@ -3,12 +3,30 @@ const Content = () => {
     const handleNameChange = () => {
     const names = ['Bishal', 'Dhiraj', 'Sachin'];
     const int = Math.floor(Math.random()*3);
-    console.log(int);
     return names[int];
   }
+
+
+  const handleClick = () => {
+      console.log('You clicked it')
+  }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was Clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+
     return (
         <main>
-            Hello {handleNameChange()}!
+            <p>
+                Hello {handleNameChange()}!
+            </p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={() => {handleClick2('Bishal')}}>Click It</button>
+            <button onClick={(e) => {handleClick3(e)}}>Click It</button>
         </main>
     );
 }
